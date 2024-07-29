95c2cdb7200fe2ee63dec0f1ab160c13bd02c908
Joey Phommasone <joey.phom@gmail.com>
2024-07-29 18:21:48 -0500

    Adding README TODO

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "joeyphom.github.io",
   server: {
     open: true
   },
